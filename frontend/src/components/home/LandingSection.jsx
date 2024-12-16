a18281cc0e9e031edf26a22189355b863dcb90f6
Lashen Martino <lashenmartino@gmail.com>
2024-12-17 01:02:58 +0530

    Create landing section & fix merege conflict

--- frontend/src/components/home/LandingSection.jsx
+++ frontend/src/components/home/LandingSection.jsx
@@ -1,29 +1,34 @@
-import contactimg from '../../assets/images/home-img/grad-img.png';
-import headerImg from '../../assets/images/home-img/header-img.png';
+import image1 from '../../assets/images/home-img/landing-img1.png';
+import image2 from '../../assets/images/home-img/landing-img2.png';
+// import img from '../../assets/images/home-img/cover.png';
 import { Link } from 'react-router-dom';
 
 export default function LandingSection() {
   return (
     <>
         <div className="relative min-h-screen overflow-x-hidden bg-custom-page">
-            {/* Contact image */}
-            <div className="relative left-20 w-full pb-4 sm:left-5 md:left-0  md:left-21 md:mb-20 xl:left-5  xl:w-[800px] xl:h-auto xl:left-[-100px] xl:bottom-16  xl:mt-0 lg:w-[650px] lg:left-50  lg:w-[400px] lg:left-80 lg:bottom-0 lg:mb-0 lg:mt-0 md:w-[550px] md:left-50 md:w-auto  md:left-[200px] md:bottom-0 md:mb-0 md:mt-0">
-                <img className="object-cover w-full h-full" src={contactimg} alt="" />
+            {/* <div className="hidden xl:block top-0 left-0 xl:right-auto xl:left-[20px] xl:bottom-auto xl:top-[20px]">
+                <img className="object-cover w-full h-[95vh]" src={img} alt="" />
+            </div> */}
+            
+            <div className="absolute hidden xl:block left-0 bottom-10 max-w-[650px] sm:max-w-[550px]">
+                <img className="object-cover w-auto h-auto" src={image1} alt="" />
             </div>
 
-            {/* Header image */}
-            <div className="hidden xl:block absolute top-0 right-0 xl:left-auto xl:right-[20px] xl:bottom-auto xl:top-[-20px]">
-                <img className="object-cover w-full h-auto" src={headerImg} alt="" />
+            <div className="hidden xl:block absolute top-0 right-0 xl:left-auto xl:right-[5px] xl:bottom-auto xl:top-[-20px]">
+                <img className="object-cover w-full h-auto" src={image2} alt="" />
             </div>
         </div>
         
-        <div className="text-center xl:absolute xl:top-1/3 xl:left-1/2 xl:transform xl:-translate-x-1/2 mb:['10px'] xl:-translate-y-1/2 xl:top-[350px]">
-            <p className="mb-4 font-serif text-2xl font-semibold text-custom-blue sm:text-5xl lg:text-5xl xl:text-6xl">
-                First collaborative hub <br />
-                For university students in <br /> Sri Lanka
+        <div className="text-center xl:absolute xl:top-1/3 xl:left-1/2 xl:transform xl:-translate-x-1/2 mb:['10px'] xl:-translate-y-1/2 xl:top-[350px] bg-transparent">
+            <p className="mb-4 font-serif text-2xl font-semibold sm:text-5xl lg:text-5xl xl:text-6xl text-blue-950">
+                Connecting Schools <br />
+                with Volunteers <br /> Spark Brighter Futures!
             </p>
             <Link to="/about-us">
-                <button className="px-6 py-3 text-white rounded-full bg-blue-950"> About Us </button>
+                <button className="px-6 py-3 text-white duration-300 bg-blue-900 rounded-full hover:scale-110 hover:bg-blue-800">
+                    About Us
+                </button>
             </Link>
         </div>
     </>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,64 +4,65 @@ import Navbar from "../components/Navbar";
 import School from "../assets/images/home-img/school.webp";
 import Volunteer from "../assets/images/home-img/volunteer.webp";
 import Organization from "../assets/images/home-img/organization.gif";
-<<<<<<< HEAD
-import ChooseUSImage from "../assets/images/home-img/choose-us.png";
-=======
+import ChooseUSImage from "../assets/images/home-img/carousel3.jpg";
 import CardSlider from "../components/home/CardSlider";
-
->>>>>>> cc31506eeb3bfd687ca0a0f9ce6b01367e473a97
+import LandingSection from "../components/home/LandingSection";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
+      <LandingSection />
       {/* <Carousel /> */}
+      
       <div className="bg-custom-page">
+        {/* <---  Start Content Section ---> */}
         <p className="p-4 italic text-center">
           We believe that we can play a major role in the journey of creating a better society. During the journey,
           we have to go through many milestones. We<br /> believe this
           will impact the future of our country.
         </p>
         <div className="flex items-center justify-center p-4 space-x-4 ">
-        <img src={School} alt="School Icon" className="h-20  border-2 border-[#3657AD] p-2 rounded-lg " />
-        <img src={Volunteer} alt="Volunteer Icon" className="h-20 border-2 border-[#3657AD] p-2 rounded-lg" />
-        <img src={Organization} alt="Organization Icon" className="h-20 border-2 border-[#3657AD] p-2 rounded-lg" />
+          <img src={School} alt="School Icon" className="h-20  border-2 border-[#3657AD] p-2 rounded-lg " />
+          <img src={Volunteer} alt="Volunteer Icon" className="h-20 border-2 border-[#3657AD] p-2 rounded-lg" />
+          <img src={Organization} alt="Organization Icon" className="h-20 border-2 border-[#3657AD] p-2 rounded-lg" />
+        </div>
         
-        </div>
-<<<<<<< HEAD
-         <h1 className="p-8 text-5xl font-bold text-center">Past Events</h1>
-         <div className="flex flex-col items-center justify-center p-6" >
-         <h1 className="font-bold text-2xl mb-4 text-center">Why Choose Us?</h1>
-          <div className="flex flex-col md:flex-row items-center gap-8">
-          <div className="w-full md:w-1/2 flex justify-center">
-            <img
-              src={ChooseUSImage}
-              alt="Why Choose Us"
-              className="w-1/2 md:w-2/3 h-auto rounded-lg shadow-lg"
-            />
+        
+        {/* <---  Past Events Section ---> */}
+        <h1 className="p-8 text-2xl font-bold text-center">Past Events</h1>
+        <CardSlider />
+        <div className="flex items-center justify-center pb-4 bg-custom-page ">
+          <button className="bg-custom-blue text-white p-3 rounded-lg border-2 border-[#EC8305]">
+            <Link to={'/past-events'}>Explore More</Link>
+          </button>
+        </div> 
+        
+        {/* <---  Why Choose Us Section ---> */}
+        <div className="flex flex-col items-center justify-center p-6">
+          <h1 className="mb-2 text-2xl font-bold text-center">Why Choose Us?</h1>
+          <div className="flex flex-col items-center md:flex-row md:gap-6 lg:gap-8">
+            <div className="w-full leading-relaxed text-gray-700 md:w-2/5 ml-[100px]">
+              <p className="mb-4 text-lg text-justify md:mb-6 lg:mb-8">
+               Our seminar volunteer program develops students leadership, collaboration, and communication abilities.
+               Students gain practical experience, boost their confidence, and refine their problem-solving skills in a nurturing 
+               environment by taking on significant responsibilities in event organization.
+              </p>
+              <p className="text-lg text-justify">
+               Furthermore, by showcasing their dedication to extracurricular activities, this platform helps students 
+               enhance their academic and professional profiles. It also offers opportunities to network with peers, 
+               instructors, and industry leaders, paving the way for future achievements and personal growth.
+              </p>
+            </div>
+            <div className="flex justify-center w-full md:w-1/2">
+              <img src={ChooseUSImage} alt="Why Choose Us" className="object-cover rounded-lg shadow-xl h-80 w-96"/>
+            </div>
           </div>
-          <div className="w-full md:w-1/2 text-gray-700 leading-relaxed">
-      <p className="text-lg mb-10 text-justify">
-        Our seminar volunteer program develops students leadership, collaboration, and communication abilities. Students get practical experience, boost their confidence, and develop their problem-solving skills in a nurturing setting by taking on significant responsibilities in event organisation. 
-        </p>
-        <p className="text-lg mb-10 text-justify">
-        Furthermore, by showcasing their dedication to extracurricular activities, this platform assists students in enhancing their academic and professional profiles. Additionally, it offers chances to network with classmates, instructors, and business leaders, opening doors for future achievement and personal development.
-        </p> 
+  
         </div>
-        </div>
-        </div>
-        </div>
-=======
-         <h1 className="p-8 text-2xl font-bold text-center">Past Events</h1>
       </div>
-      <CardSlider />
-      <div className="flex items-center justify-center pb-4 bg-custom-page ">
-  <button className="bg-[#3657AD] text-white p-2 rounded-lg">
-    Explore More
-  </button>
-</div>
       
->>>>>>> cc31506eeb3bfd687ca0a0f9ce6b01367e473a97
       <Footer />
     </>
   )
