a1dc0ea1161ad0a21487d76cf8151bb9cb1bc902
Thiseni Perera <pererathiseni@gmail.com>
2024-12-23 21:18:14 +0530

    Updated AboutUs.jsx

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -11,8 +11,8 @@ function AboutUs() {
   
           {/* Card 1 */}
           <div className="flex items-center max-w-7xl ml-150px m-4 rounded-lg shadow-md bg-custom-orange">
-            <p className="text-[30px] font-bold text-center text-black">
-              &quot;Volunteering is the ultimate expression of compassion in action, making a difference one act at a time.&quot;
+            <p className="text-2xl font-bold text-center text-black">
+              &quot;Volunteering is the ultimate expression of compassion in action, making a difference <br/> one act at a time.&quot;
             </p>
           </div>
   
@@ -24,11 +24,12 @@ function AboutUs() {
               alt="About Us"
             />
             <div className="flex flex-col items-center justify-center">
-              <h2 className="mb-8 text-4xl font-bold text-center text-black ">About Us</h2>
-              <p className="text-2xl leading-relaxed text-center text-black">
+              <h2 className="mb-8 text-2xl font-bold text-center text-black ">About Us</h2>
+              <p className="text-lg leading-relaxed text-center text-black">
               LearnHeart unites volunteers and organizations to bridge educational gaps in rural Sri Lanka.
-               Through project matching and seamless collaboration, we’re creating equal learning opportunities
-                and building brighter futures. Join us in making a difference!
+              Through project matching and seamless collaboration, we’re creating equal learning opportunities
+              and building brighter futures. We believe in the power of education to transform lives and build brighter futures.<br/>
+              Join us in creating a lasting impact on education!
               </p>
             </div>
           </div>
@@ -41,17 +42,89 @@ function AboutUs() {
               alt="Vision"
             />
             <div className="flex flex-col items-center justify-center">
-              <h2 className="mb-8 text-4xl font-bold text-center text-white">Our Vision</h2>
-              <p className="text-2xl leading-relaxed text-center text-white">
+              <h2 className="mb-8 text-2xl font-bold text-center text-white">Our Vision</h2>
+              <p className="text-lg leading-relaxed text-center text-white">
               Our vision is to create equal learning opportunities for every child, empowering communities
-              through education and meaningful connections.
+              through education and meaningful connections. Together, we 𝘫𝘰𝘪𝘯 𝘩𝘢𝘯𝘥𝘴 𝘵𝘰 𝘪𝘨𝘯𝘪𝘵𝘦 𝘮𝘪𝘯𝘥𝘴  to uplift communities and create a more
+              inclusive and equitable world to believe in the transformative power of education.
               </p>
             </div>
+          </div>
+
+          {/* Card 4 */}
+          <div className="p-5 text-center bg-blue-50 mt-16 relative">
+            <h1 className="-mb-20 text-2xl font-bold text-black">Why<br></br>LearnHeart?</h1> 
+          
+          {/* SVG Lines */}
+          <svg
+            className="absolute w-full h-full"
+            viewBox="0 0 800 200"
+            preserveAspectRatio="xMidYMid meet"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line
+              x1="120"
+              y1="75"
+              x2="250"
+              y2="170"
+              stroke="#3657AD"
+              strokeWidth="3"
+            />
+            <line
+              x1="330"
+              y1="153"
+              x2="459"
+              y2="58"
+              stroke="#3657AD"
+              strokeWidth="3"
+            />
+            <line
+              x1="650"
+              y1="150"
+              x2="500"
+              y2="67"
+              stroke="#3657AD"
+              strokeWidth="3"
+            />
+          </svg>
+            
+            <div className="flex items-center justify-center p-10 gap-28">
+              <div className="flex flex-col items-center">
+                <div className="flex items-center justify-center w-32 h-32 text-3xl rounded-full text-custom-orange bg-custom-blue">
+                  <i className="fas fa-chalkboard items-center text-4xl"></i>
+                </div>
+                <p className="mt-2 text-[16px] text-black">Request School<br></br>Seminars</p>
+              </div>
+              <div className="flex flex-col items-center mt-72">
+                <div className="flex items-center justify-center w-40 h-40 text-3xl rounded-full text-custom-orange bg-custom-blue">
+                  <i className="fas fa-user-friends items-center text-6xl"></i>
+                </div>
+                <p className="mt-2 text-[16px] text-black">Help Students</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <div className="flex items-center justify-center w-32 h-32 text-3xl rounded-full text-custom-orange bg-custom-blue">
+                  <i className="fas fa-network-wired text-4xl"></i>
+                </div>
+                <p className="mt-2 text-[16px] text-black">Connect with<br></br>Volunteers</p>
+              </div>
+              <div className="flex flex-col items-center mt-72">
+                <div className="flex items-center justify-center w-40 h-40 text-3xl rounded-full text-custom-orange bg-custom-blue">
+                  <i className="fas fa-book text-6xl"></i>
+                </div>
+                <p className="mt-2 text-[16px] text-black">Access to<br></br>Resources</p>
+              </div>
+            </div>
+          </div>
+
+
+
+
+
           </div>
   
             
           
-        </div>
+        
         <Footer />  
       </>
     );
