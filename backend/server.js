c364ee0fb3a8c78dfc44aecf092e28abf827e52e
malindu29 <malindu1067@gmail.com>
2025-02-02 14:49:23 +0530

    Updated some changes in resourceBank-backend

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const dotenv = require('dotenv').config();
 const app = express();
 const PORT = 3001;
 
+const organizationRoutes = require('./routes/organizationRoutes');
+const volunteerRoutes = require('./routes/volunteerRoutes');
+const seminarRoutes = require('./routes/seminarRoutes');
 const resourceRoutes = require('./routes/resourceRoutes');
 
+// Middlewares
 app.use(express.json());
+app.use(cors());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+})
 
 // Connecting database
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('Database connected'))
   .catch(err => console.log('Database not connected', err));
 
-// Connecting port
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
 });
 
+// Routes
+app.use('/api/organizations', organizationRoutes);
+app.use('/api/volunteers', volunteerRoutes);
+app.use('/api/seminars', seminarRoutes);
 app.use('/api/resources', resourceRoutes);
