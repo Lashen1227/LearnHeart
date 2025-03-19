3dc84cb6a6d919296717d718f2993396e43c76e1
Lashen Martino <lashenmartino@gmail.com>
2025-03-19 19:59:14 +0530

    Refactor import statements in App.jsx for consistent formatting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import HomePage from './pages/HomePage';
-import { SpeedInsights } from '@vercel/speed-insights/react';
+import HomePage from './pages/HomePage'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
