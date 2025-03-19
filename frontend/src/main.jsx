bed7f2d38b597be84069631ded6d7f040fc8a8a9
Lashen Martino <lashenmartino@gmail.com>
2025-03-19 14:14:50 +0530

    Change router to use HashRouter for improved routing compatibility

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import { HashRouter as createBrowserRouter, RouterProvider, } from "react-router-dom";
 import { ClerkProvider } from '@clerk/clerk-react';
 
 // Landing Pages
